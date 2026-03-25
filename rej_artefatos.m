f474ea21825da49508ecb98c2101c8d022688ec1
gcdemelo <chaves1135@gmail.com>
2026-03-25 16:07:07 -0300

    Update rej_artefatos.m

--- rej_artefatos.m
+++ rej_artefatos.m
@@ -15,15 +15,15 @@ while length(eeg(:,1)) ~= num_trials
     dp = var(eeg,0,2).^0.5; % (A,w,dim) => w = 0 => n-1  ...  w = 1 => n
     dp_medio = mean(dp);
     num_trials = length(eeg(:,1)); % iguala num_trials ao tamanho instantaneo do eeg
-    linhas_remanecentes = ones(1,num_trials);
+    linhas_remanescentes = ones(1,num_trials);
     % se dentro do "for" o tamanho do eeg nao mudar, a funcao tem que encerrar
     for i = 1:length(eeg(:,1)) % esses limites sao definidos quando entra no for e nao muda - eh o 1o valor calculado que fica
         if dp(i) > 2*dp_medio            
             trials_eliminados = trials_eliminados + 1;
-            linhas_remanecentes(i) = 0; % linha na posicao i vai ser eliminada                       
+            linhas_remanescentes(i) = 0; % linha na posicao i vai ser eliminada                       
         end
     end
-    eeg = eeg(logical(linhas_remanecentes),:);
+    eeg = eeg(logical(linhas_remanescentes),:);
 end
 
 end
